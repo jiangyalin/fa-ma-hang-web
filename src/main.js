ae7d0073b4ee0316db4ab5497db468a1491ed29a
蒋亚林 <18725944157@163.com>
2018-01-16 18:46:30 +0800

    架构

--- src/main.js
+++ src/main.js
@@ -4,11 +4,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import VueI18n from 'vue-i18n'
 import VueCookie from 'vue-cookie'
-import locales from './i18n'
-import VeLine from 'v-charts/lib/line'
-import VePie from 'v-charts/lib/pie'
 import config from './config'
 import publicFunction from './public'
 import moment from 'moment'
@@ -19,15 +15,11 @@ import 'font-awesome/css/font-awesome.min.css'
 import './style/app/app.scss'
 import App from './App.vue'
 
-Vue.use(VueI18n)
 Vue.component('full-calendar', fullCalendar)
 Vue.use(VueCookie)
 Vue.use(ElementUI)
 Object.defineProperty(Vue.prototype, '$moment', { value: moment })
 
-Object.keys(locales).forEach(function (lang) {
-  Vue.locale(lang, locales[lang])
-})
 window.moment = moment
 
 window.config = config
@@ -35,9 +27,6 @@ window.config = config
 window.publicFunction = publicFunction
 
 Vue.config.lang = 'zh-cn'
-
-Vue.component(VeLine.name, VeLine)
-Vue.component(VePie.name, VePie)
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
